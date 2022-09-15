8c57c46135f5cef587dda886ed97e0578455c894
MiroslavKop <kopytovmiroslav@gmail.com>
2022-09-15 17:21:27 +0300

    getCash func

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function getCash(amount) {
+  const banknotes = [500, 200, 100, 50, 20, 5, 2, 1];
+  let newArr = [];
+  if (amount > 0) {
+    for (let i = 0; i < banknotes.length; i++) {
+      amount -= banknotes[i];
+      newArr.push(banknotes[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(getCash(874)); // [500, 200, 100, 50, 20, 2, 2
+console.log(getCash(1502));
